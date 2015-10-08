addf5681657d4c35437e7f0bdb4001a3022f77be
sergiotorres81 <sergiotorres81@gmail.com>
2015-10-08 17:56:42 +0300

    added initial structure

---
+++ src/main/java/com/ssrr/StockScreenerRemoteRepoApplication.java
@@ -0,0 +1,12 @@
+package com.ssrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockScreenerRemoteRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockScreenerRemoteRepoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ssrr/domain/Stock.java
@@ -0,0 +1,5 @@
+package com.ssrr.domain;
+
+public class Stock {
+
+}
